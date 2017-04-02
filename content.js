c67583f82838a7ae170703734fc9553cf1405814
kubo25 <jakubhucko@gmail.com>
2017-04-02 17:42:38 +0200

    Fixed regex error

--- content.js
+++ content.js
@@ -8,8 +8,10 @@ document.addEventListener("contextmenu", function(e){
         var img = e.target;
         var style = img.currentStyle || window.getComputedStyle(img, false);
         var url = style.backgroundImage.slice(4, -1);
-                
-        if((/\.(gif|jpg|jpeg|tiff|tif|png|bmp|webp)$/).test(url)){
+        
+        console.log(url);
+        
+        if((/\.(gif|jpg|jpeg|tiff|tif|png|bmp|webp)/).test(url)){
             message = url.slice(1, -1);
         }
         else{
